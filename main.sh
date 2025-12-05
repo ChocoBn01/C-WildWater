cac1a67839df1d86c71992cd7067279cb39b7b39
Diego Delvig <157409019+DiegoDelvig@users.noreply.github.com>
2025-12-05 08:53:19 +0100

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$1" == "histo" ] && [ "$2" == "src" ]; then
-    grep "Spring" data/c-wildwater_v0.dat | cut -d';' -f3- | cut -d'#' -f2 | cut -d';' -f1,2 | tr ';' ' ' >> tests/histo_src.txt
+
+    grep "Spring" data/c-wildwater_v0.dat | cut -d';' -f3- | cut -d'#' -f2 | cut -d';' -f1,2 | tr ';' '\n' >> tests/histo_srx_nom.txt
 else
     echo "Cette commande n'est pas reconnue."
 fi
